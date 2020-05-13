4a213b80061f4795cc4aeeeb30c11ca65aa7b097
Jesper Voldby Hansen <jesp58c0@edu.easj.dk>
2020-05-13 11:39:49 +0200

    Tilføjet booking modelklasse

---
+++ RestService/ModelLib/Model/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; 
+namespace ModelLib.Model
+{ 
+    public class Booking 
+    { 
+        private string _flightNr;
+        private int _seatNr;
+        private string _travelClass;
+        private bool _allReadyBooked;
+        public Booking(string flightNr, int seatNr, string travelClass, bool allReadyBooked) 
+        {
+            FlightNr = flightNr;
+            SeatNr = seatNr;
+            TravelClass = travelClass;
+            AllReadyBooked = allReadyBooked;
+        }
+        public string FlightNr { get; set; }
+        public int SeatNr { get; set; }
+        public string TravelClass { get; set; }
+        public bool AllReadyBooked { get; set; }
+    }
+}
